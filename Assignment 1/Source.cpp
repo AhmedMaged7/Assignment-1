f3b4788c673eb4dd8074ba595ebcd581bc07ed50
AhmedMaged7 <ahmedmaged77@hotmail.com>
2017-12-28 17:32:33 +0200

    Add files via upload

---
+++ Assignment 1/Source.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+using namespace std;
+
+struct node
+{
+	int value;
+	node* next;
+};
+node * head = 0;
+
+void reverse()
+{
+	node * prev = 0;
+	node * temp = head;
+	node * next;
+	while (temp != 0)
+	{
+		next = temp->next;
+		temp->next = prev;
+		prev = temp;
+		temp = next;
+	}
+	head = prev;
+}
+
+
+
+void print() {
+
+	node *temp = head;
+
+	while (temp != 0) {
+
+		cout << temp->value << "  ";
+		temp = temp->next;
+
+	}
+}
+
+	void insert(){
+
+		node * newnode = new node;
+
+		cin >> newnode->value;
+		newnode->next = 0;
+
+		if (head == 0) head = newnode;
+		else {
+			node * temp = head;
+			while (temp->next != NULL)
+			{
+				temp = temp->next;
+			}
+				temp->next = newnode;
+			
+
+		}
+	}
+
+	int main()
+	{
+		insert();
+		insert();
+		insert();
+		cout << "Original" << endl;
+		print();
+		cout << endl;
+		cout << "Reversed" << endl;
+		reverse();
+		print();
+		
+	}
